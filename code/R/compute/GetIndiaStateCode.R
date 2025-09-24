f5d0233192b827b116badf1cb3bf7957f0123a16
qiqi <108138869+loofla@users.noreply.github.com>
2025-09-24 21:36:20 +0800

    Update GetIndiaStateCode.Ra
    add comments

--- code/R/compute/GetIndiaStateCode.R
+++ code/R/compute/GetIndiaStateCode.R
@@ -1,4 +1,17 @@
-
+###############################################################################
+# Levels and trends in the sex ratio at birth and missing female births 
+# for 29 states and union territories in India 1990–2016: A Bayesian modeling study
+#
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 24 September 2025
+# 
+# GetIndiaStateCode.R
+# 
+# This script contains all functions related to getting India State code.
+# Functions are: function1(.., function2(3), ..); means function2 is called
+# three times inside function1.
+# GetIndiaStateCode(..)
+###############################################################################
 GetIndiaStateCode <- function(name.in) {
   
   ## get India state code names ##
