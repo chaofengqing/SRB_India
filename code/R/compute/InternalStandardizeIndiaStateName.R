f091a22805328e751185c07a5656bbe5f591e68e
qiqi <108138869+loofla@users.noreply.github.com>
2025-09-24 21:38:42 +0800

    Update InternalStandardizeIndiaStateName.R
    add comments

--- code/R/compute/InternalStandardizeIndiaStateName.R
+++ code/R/compute/InternalStandardizeIndiaStateName.R
@@ -1,4 +1,18 @@
-
+###############################################################################
+# Levels and trends in the sex ratio at birth and missing female births 
+# for 29 states and union territories in India 1990–2016: A Bayesian modeling study
+#
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 24 September 2025
+# 
+# InternalStandardizeIndiaStateName.R
+# 
+# This script contains all functions related to shortening India's region name.
+# Functions are: function1(.., function2(3), ..); means function2 is called
+# three times inside function1.
+# InternalStandardizeNepalDevRegName(..)
+###############################################################################
+#------------------------------------------------------------------------------
 
 InternalStandardizeIndiaStateName <- function(name.in) {
   
